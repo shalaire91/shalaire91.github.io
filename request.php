ff466748944d1f12e3002b997b64b8ee2ce96a31
shalaire <shalaire91@gmail.com>
2018-12-13 08:41:59 +0800

    2nd website

---
+++ request.php
@@ -0,0 +1,16 @@
+<?php 
+if(isset(@_POST['submit'])){
+	$firstName = $_POST['txtFirstName'];
+	$lastName = $_POST['txtLastName'];
+	$emailAddress = $_POST['txtEmailAddress'];
+	$subject =  $_POST['txtSubject'];
+	$message = $_POST['txtMessage'];
+	
+	$send_email_to = "shalaire91@gmail.com";
+	$headers .= "From: ".$emailAddress. "\r\n";
+	 $txt = "You have recieved an e-mail from".$firstName."."$lastName".\n\n."$message;
+	
+	mail($send_email_to, $email_subject, $message,$headers);
+	header("Location: send_form_email.php?mailsend");
+}
+?>
---
+++ send_form_email.php
@@ -0,0 +1,17 @@
+<?php 
+if(isset(@_POST['submit'])){
+	$firstName = $_POST['txtFirstName'];
+	$lastName = $_POST['txtLastName'];
+	$emailAddress = $_POST['txtEmailAddress'];
+	$subject =  $_POST['txtSubject'];
+	$message = $_POST['txtMessage'];
+	$txtart = $_POST['txtart'];
+	
+	$send_email_to = "shalaire91@gmail.com";
+	$headers .= "From: ".$emailAddress. "\r\n";
+	$txt = "You have recieved an e-mail from".$firstName."."$lastName".\n\n."$txtart."."$message;
+	
+	mail($send_email_to, $email_subject, $message, $txtart, $headers);
+	header("Location: send_form_email.php?mailsend");
+}
+?>
